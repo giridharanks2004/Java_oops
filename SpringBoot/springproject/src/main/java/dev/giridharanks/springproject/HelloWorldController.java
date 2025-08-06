7c001ee64410280c6a91531e2667e7e24198eaaa
giridharanks2004 <iamgiridharanks@gmail.com>
2025-08-06 17:29:22 +0530

    update: todobasics

--- SpringBoot/springproject/src/main/java/dev/giridharanks/springproject/HelloWorldController.java
+++ SpringBoot/springproject/src/main/java/dev/giridharanks/springproject/HelloWorldController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    @GetMapping("/hello")
+    @GetMapping("/h")
     public String Greeting(){
         return "hello world";
     }
---
+++ SpringBoot/springproject/src/main/java/dev/giridharanks/springproject/TodoController.java
@@ -0,0 +1,44 @@
+package dev.giridharanks.springproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/todo") // maping a common request to it's sub requests
+public class TodoController {
+    @GetMapping("/1") // sub requests
+    public String todo(){
+        return "todo";
+    }
+
+    @GetMapping("/{id}") // path variable
+    public String todobyid(@PathVariable int id){
+        return "todo id "+id;
+    }
+    // request param custom name
+    @GetMapping("/")
+    public String todobyidreq(@RequestParam("todoid") int id){
+        if(id == 20){
+            return "id is matched!";
+        }
+        return "id mismatch";
+    }
+    //user id pwd
+    @GetMapping("/create")
+    public String creatac(@RequestParam String uid,@RequestParam String pass){
+        if(uid.equals("giri") && pass.equals("ksg")){
+            return "welcome home " + uid;
+        }
+        return "poda veliya";
+    }
+
+    @PostMapping("/createac")
+    public String createacSec(@RequestBody String input){
+        return input;
+    }
+}
